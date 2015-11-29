df6a9473a16c3bc8a83d739d5ad4f4612478df38
DoDevCodenameLambda <dodo2998@gmail.com>
2015-11-29 16:48:57 +0100

    Gerüst

--- PSServe.py
+++ PSServe.py
@@ -1,1 +1,18 @@
-__author__ = 'MaxMustermann'
+import socket
+from _thread import start_new_thread
+
+
+class Server(object):
+    def __init__(self, port=0x7079):
+        self.port = port
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.bind((socket.gethostname(), self.port))
+        self.socket.listen(5)
+        self.handler = lambda addr, sock: True
+        while True:
+            (clientsocket, address) = self.socket.accept()
+            start_new_thread(self.handler, (address, clientsocket))
+
+    def use(self, handler):
+        self.handler = handler
+        return handler
